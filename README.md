2f8bcbce99be7570165b06400eac12edf4c8ea65
YUhari0901 <119827528+YUhari0901@users.noreply.github.com>
2023-02-26 07:35:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-product-purchage-card
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5hppsj)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>GHL Wordl!</h1>
+      <p>Georgy, Hari et Loi :)</p>
+    </div>
+  );
+}
