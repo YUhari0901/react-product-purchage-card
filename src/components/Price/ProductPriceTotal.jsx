d629372c204c691378e39c64510b1fb9ec316237
YUhari0901 <119827528+YUhari0901@users.noreply.github.com>
2023-02-26 20:45:43 +0900

    purchageButton 수량 수정

--- src/components/Price/ProductPriceTotal.jsx
+++ src/components/Price/ProductPriceTotal.jsx
@@ -6,17 +6,24 @@ const ProductPriceTotal = () => {
   // const [count, price, sale] = useState();
 
   let PriceTotal = {
-    count: 3,
+    count: 1,
     price: 6370,
   };
 
   return (
     <div className={styles.priceTotalContainer}>
-      <span className={styles.sum}>총 상품 금액
-      <button className={styles.helpButton}>?<span className={styles.a11y}>도움말 버튼</span> </button>
+      <span className={styles.sum}>
+        총 상품 금액
+        <button className={styles.helpButton}>
+          ?<span className={styles.a11y}>도움말 버튼</span>{' '}
+        </button>
       </span>
-      <span className={styles.amount}>{'총 수량 ' + PriceTotal.count + '개'}</span>
-      <span className={styles.sumTotal}>{PriceTotal.price * PriceTotal.count + '원'}</span>
+      <span className={styles.amount}>
+        {'총 수량 ' + PriceTotal.count + '개'}
+      </span>
+      <span className={styles.sumTotal}>
+        {PriceTotal.price * PriceTotal.count + '원'}
+      </span>
     </div>
   );
 };
