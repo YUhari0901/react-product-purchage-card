64cb51d57c7e7f19d95554f4a89c0681f3774f80
YUhari0901 <119827528+YUhari0901@users.noreply.github.com>
2023-02-26 13:20:57 +0900

    PurchageView 완성

--- src/components/Price/ProductPrice.jsx
+++ src/components/Price/ProductPrice.jsx
@@ -1,10 +1,23 @@
 import styles from './Price.module.css';
 import React from 'react';
 
-const ProductPrice = () => {
+const ProductPrice = ({ sale, priceNormal, priceReduced }) => {
+  let price = { sale: '88%', priceOrigin: '56,760원', priceReduced: '6,370원' };
+
   return (
-    <div>
-      <h2>상품가격</h2>
+    <div className={styles.priceContainer}>
+      <span className={styles.sale}>
+        <span className={styles.a11y}>할인율</span>
+        {price.sale}
+      </span>
+      <span className={styles.priceOrigin}>
+        <span className={styles.a11y}>정가</span>
+        {price.priceOrigin}
+      </span>
+      <span className={styles.priceReduced}>
+        <span className={styles.a11y}>할인된 금액</span>
+        {price.priceReduced}
+      </span>
     </div>
   );
 };
--- src/components/PurchageView/PurchageView.jsx
+++ src/components/PurchageView/PurchageView.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import productImage from '../../assets/productImage.svg';
 
 const PurchageView = ({ name, amount }) => {
-  let product = { name: '레피젠 코로나 자가 검사키트', amount: '2개입X1박스' };
+  let product = { name: '래피젠 코로나 자가 검사 키트', amount: '2개입X1박스' };
 
   return (
     <>
       <h2 className={style.a11y}>상품이미지와 상품명</h2>
-      <img src={productImage} alt="레피젠 코로나 자가 검사키트 이미지" />
-      <div>
+      <div className={style.imageContainer}>
+        <img src={productImage} alt="래피젠 코로나 자가 검사 키트 이미지" />
+      </div>
+      <div className={style.productNameAmount}>
         <strong>
           {product.name}
           <br />
