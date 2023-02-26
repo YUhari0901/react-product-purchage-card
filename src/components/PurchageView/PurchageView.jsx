8deeaf0e24c65d070ad6c6705c0a5d05ae880cb0
YUhari0901 <119827528+YUhari0901@users.noreply.github.com>
2023-02-26 11:13:01 +0900

    1 binaries uploaded

--- src/components/PurchageView/PurchageView.jsx
+++ src/components/PurchageView/PurchageView.jsx
@@ -5,6 +5,10 @@ const PurchageView = () => {
   return (
     <div>
       <h2>상품이미지</h2>
+      <img
+        src="../../assets/productImage.svg"
+        alt="레피젠 코로나 자가 검사키트 이미지"
+      />
     </div>
   );
 };
